743ee53616a0234e50908997fc6e7771f04fd625
asmodat <asmodat@gmail.com>
2020-05-01 22:20:43 +0200

    no message

--- docker/kira-alpha/relayer/start.sh
+++ docker/kira-alpha/relayer/start.sh
@@ -25,86 +25,25 @@ rly cfg init || true
 
 # python3 $RELAY_SCRIPS/phase1.py "$BASECHAIN_JSON_PATH" "$RLYKEY_MNEMONIC" "$GOZCHAIN_JSON_PATH" "$RLYKEY_MNEMONIC"
 
-<< ////
 
-GOZCHAIN_JSON_PATH=${RELAY_CONFIGS}/kira-1.json
-
-cat > $GOZCHAIN_JSON_PATH << EOL
-{
-  "key": "faucet",
-  "chain-id": "kira-1",
-  "rpc-addr": "http://goz.kiraex.com:10001",
-  "account-prefix": "cosmos",
-  "gas": 200000,
-  "gas-prices": "0.025ukex",
-  "default-denom": "ukex",
-  "trusting-period": "330h"
-}
-EOL
-
-GOZCHAIN_ID=$(cat $GOZCHAIN_JSON_PATH | jq -r '."chain-id"')
-
-# connect directly and claim goz tokens
-rly ch add -f $GOZCHAIN_JSON_PATH
-rly keys restore $GOZCHAIN_ID chain_key_$GOZCHAIN_ID "$RLYKEY_MNEMONIC"
-rly lite init $GOZCHAIN_ID -f
-rly tst request $GOZCHAIN_ID chain_key_$GOZCHAIN_ID
-
-rly q bal $CHAIN_ID
-rly q bal $GOZCHAIN_ID
-
-# connect 2 chains
-rly pth gen $CHAIN_ID transfer $GOZCHAIN_ID transfer ${CHAIN_ID}_${GOZCHAIN_ID}
-rly pth show ${CHAIN_ID}_${GOZCHAIN_ID} -j
-rly tx link ${CHAIN_ID}_${GOZCHAIN_ID}
-
-# send 2kex from alpha to goz
-rly tx transfer $CHAIN_ID $GOZCHAIN_ID 2kex true $(rly ch addr $GOZCHAIN_ID)
-# send 1kex from goz to alpha
-rly tx transfer $GOZCHAIN_ID $CHAIN_ID  1kex true $(rly ch addr $CHAIN_ID)
-
-rly q bal $CHAIN_ID
-rly q bal $GOZCHAIN_ID
-////
-
-
-# rly pth gen $CHAIN_ID transfer $GOZCHAIN_ID transfer ${CHAIN_ID}_${GOZCHAIN_ID}
-# rly tx link ${CHAIN_ID}_${GOZCHAIN_ID}
-# 
-# rly q bal chainylira
-# rly pth gen $CHAIN_ID transfer chainylira transfer ${CHAIN_ID}_chainylira
-# rly tx link ${CHAIN_ID}_chainylira
-# 
-# Balance of chain_key_ 
-# [{'denom': 'nickel', 'amount': '1000000'}]
-# Connecting kira-alpha and chainylira...
-# 
-# rly pth gen kira-alpha transfer hashquarkchain transfer kira-alpha_hashquarkchain
-# rly tx link kira-alpha_hashquarkchain
-# 
-# rly transact link kira-alpha_chainylira -o=600s
-
-####################
-#rly ch add -f $BASECHAIN_JSON_PATH
-#
-## create a local rly key for the chain
-#rly keys add $CHAIN_ID testkey
-#
-## confiure the chain to use that key by default
-#rly ch edit $CHAIN_ID key testkey
-#
-## initialize the lite client for {{chain_id}}
-#rly lite init $CHAIN_ID -f
-#
-## request funds from the faucet to test it
-#rly tst request $CHAIN_ID testkey
-#
-## lookup balance for the rly key now
+#GOZCHAIN_ID=$(cat $GOZCHAIN_JSON_PATH | jq -r '."chain-id"')
+## connect directly and claim goz tokens
+#rly ch add -f $GOZCHAIN_JSON_PATH
+#rly keys restore $GOZCHAIN_ID chain_key_$GOZCHAIN_ID "$RLYKEY_MNEMONIC"
+#rly lite init $GOZCHAIN_ID -f
+#rly tst request $GOZCHAIN_ID chain_key_$GOZCHAIN_ID
 #rly q bal $CHAIN_ID
-#####################
-
-#python3 $RELAY_SCRIPS/relay.py "$RLYS_HOME"
-
+#rly q bal $GOZCHAIN_ID
+## connect 2 chains
+#rly pth gen $CHAIN_ID transfer $GOZCHAIN_ID transfer ${CHAIN_ID}_${GOZCHAIN_ID}
+#rly pth show ${CHAIN_ID}_${GOZCHAIN_ID} -j
+#rly tx link ${CHAIN_ID}_${GOZCHAIN_ID}
+## send 2kex from alpha to goz
+#rly tx transfer $CHAIN_ID $GOZCHAIN_ID 2kex true $(rly ch addr $GOZCHAIN_ID)
+## send 1kex from goz to alpha
+#rly tx transfer $GOZCHAIN_ID $CHAIN_ID  1kex true $(rly ch addr $CHAIN_ID)
+#rly q bal $CHAIN_ID
+#rly q bal $GOZCHAIN_ID
 
 
 echo "Relayer Node init STOP"
--- docker/kira-goz/relayer/start.sh
+++ docker/kira-goz/relayer/start.sh
@@ -21,26 +21,6 @@ chmod 777 -R $RLYS_HOME
 
 rly cfg init || true
 
-
-####################
-#rly ch add -f $BASECHAIN_JSON_PATH
-#
-## create a local rly key for the chain
-#rly keys add $CHAIN_ID testkey
-#
-## confiure the chain to use that key by default
-#rly ch edit $CHAIN_ID key testkey
-#
-## initialize the lite client for {{chain_id}}
-#rly lite init $CHAIN_ID -f
-#
-## request funds from the faucet to test it
-#rly tst request $CHAIN_ID testkey
-#
-## lookup balance for the rly key now
-#rly q bal $CHAIN_ID
-#####################
-
 #python3 $RELAY_SCRIPS/relay.py "$RLYS_HOME"
 
 echo "Relayer Node init STOP"
